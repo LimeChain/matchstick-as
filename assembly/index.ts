ede0480157d84e6cf953dbea41944360b789e5db
georg-getz <georgi.getz@outlook.com>
2021-06-21 13:36:37 +0300

    testing connectivity

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,5 +1,19 @@
+export const map = new Map<string, string>();
+
 // TODO: pass the name parameter to Rust for logging
 export function test(_name: string, f: () => void): bool {
   f();
   return true;
+}
+
+export function mockFunction(fnName: string, param: string, expectedReturnValue: string): void {
+  map.set(fnName + param, expectedReturnValue);
+}
+
+export function callFunction(fnName: string, param: string): string {
+  const hash = fnName + param;
+  if (map.has(hash)) {
+    return map.get(hash);
+  }
+  throw new Error("No function with hash " + hash + " found.");
 }
