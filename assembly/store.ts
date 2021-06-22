22da66bf747574c051d35677391f4ccb0b038fd5
georg-getz <georgi.getz@outlook.com>
2021-06-22 16:51:08 +0300

    made functions public

--- assembly/store.ts
+++ assembly/store.ts
@@ -3,34 +3,34 @@ import { Entity } from "@graphprotocol/graph-ts";
 let storeMap = new Map<string, Map<string, Entity>>();
 
 export namespace store {
-    function get(entityType: string, id: string): Entity | null {
+    export function get(entityType: string, id: string): Entity | null {
         if (storeMap.has(entityType)) {
             return storeMap.get(entityType).get(id);
         }
         throw new Error("Following type is absent from map: " + entityType);
     }
 
-    function set(entityType: string, id: string, data: Entity): void {
+    export function set(entityType: string, id: string, data: Entity): void {
         if (!storeMap.has(entityType)) {
             storeMap.set(entityType, new Map);
         }
         storeMap.get(entityType).set(id, data);
     }
 
-    function remove(entityType: string, id: string): void {
+    export function remove(entityType: string, id: string): void {
         if (storeMap.has(entityType) && storeMap.get(entityType).has(id)) {
             storeMap.get(entityType).delete(id);
         }
     }
 
-    function assertFieldEq(entityType: string, id: string, fieldName: string, expectedVal: string): bool {
+    export function assertFieldEq(entityType: string, id: string, fieldName: string, expectedVal: string): bool {
         if (storeMap.has(entityType) && storeMap.get(entityType).has(id) && storeMap.get(entityType).get(id).get(fieldName) != null) {
             return storeMap.get(entityType).get(id).get(fieldName)?.toString() == expectedVal
         }
         return false;
     }
 
-    function clear(): void {
+    export function clear(): void {
         storeMap.clear();
     }
 }
