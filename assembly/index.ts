618497acbbae44dc2f970228178300df35fd3446
Petko <pavlovskipetko@abv.bg>
2021-06-24 15:50:03 +0300

    get log methods locally

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,4 +1,37 @@
-import { log } from "../node_modules/@graphprotocol/graph-ts";
+export declare namespace log {
+  // Host export for logging, providing basic logging functionality
+  export function log(level: Level, msg: string): void
+}
+
+export namespace log {
+  export enum Level {
+    CRITICAL = 0,
+    ERROR = 1,
+    WARNING = 2,
+    INFO = 3,
+    DEBUG = 4,
+  }
+
+  export function critical(msg: string, args: Array<string>): void {
+    log(Level.CRITICAL, format(msg, args))
+  }
+
+  export function error(msg: string, args: Array<string>): void {
+    log(Level.ERROR, format(msg, args))
+  }
+
+  export function warning(msg: string, args: Array<string>): void {
+    log(Level.WARNING, format(msg, args))
+  }
+
+  export function info(msg: string, args: Array<string>): void {
+    log(Level.INFO, format(msg, args))
+  }
+
+  export function debug(msg: string, args: Array<string>): void {
+    log(Level.DEBUG, format(msg, args))
+  }
+}
 
 let map = new Map<i32, string>();
 // TODO: pass the name parameter to Rust for logging
@@ -43,3 +76,4 @@ function createHash(address: string, fnName: string, fnArguments: string[]): i32
     
   return hash;
 }
+
