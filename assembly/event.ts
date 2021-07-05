3eb868f7dbc52fa428c6082e37a0bf6756d223ec
Petko <pavlovskipetko@abv.bg>
2021-07-05 13:14:40 +0300

    Fix mock value for h160

--- assembly/event.ts
+++ assembly/event.ts
@@ -4,21 +4,20 @@ import {
   Address,
   BigInt,
   EthereumBlock,
-  Bytes,
   typeConversion,
   EthereumTransaction,
   EthereumValue,
   EthereumValueKind
-} from "@graphprotocol/graph-ts";
+} from "../node_modules/@graphprotocol/graph-ts";
 
-let defaultBlock: EthereumBlock;
-defaultBlock.hash = typeConversion.stringToH160("1");
-defaultBlock.parentHash = typeConversion.stringToH160("1");
-defaultBlock.unclesHash = typeConversion.stringToH160("1");
+let defaultBlock: EthereumBlock = new EthereumBlock();
+defaultBlock.hash = typeConversion.stringToH160("0x111");
+defaultBlock.parentHash = typeConversion.stringToH160("0x111");
+defaultBlock.unclesHash = typeConversion.stringToH160("0x111");
 defaultBlock.author = Address.fromString("123456");
-defaultBlock.stateRoot = typeConversion.stringToH160("1");
-defaultBlock.transactionsRoot = typeConversion.stringToH160("1");
-defaultBlock.receiptsRoot = typeConversion.stringToH160("1");
+defaultBlock.stateRoot = typeConversion.stringToH160("0x111");
+defaultBlock.transactionsRoot = typeConversion.stringToH160("0x111");
+defaultBlock.receiptsRoot = typeConversion.stringToH160("0x111");
 defaultBlock.number = BigInt.fromI32(1);
 defaultBlock.gasUsed = BigInt.fromI32(1);
 defaultBlock.gasLimit = BigInt.fromI32(1);
@@ -27,28 +26,28 @@ defaultBlock.difficulty = BigInt.fromI32(1);
 defaultBlock.totalDifficulty = BigInt.fromI32(1);
 defaultBlock.size = BigInt.fromI32(1);
 
-let defaultTransaction: EthereumTransaction;
-defaultTransaction.hash = typeConversion.stringToH160("1");
+let defaultTransaction: EthereumTransaction = new EthereumTransaction();
+defaultTransaction.hash = typeConversion.stringToH160("0x111");
 defaultTransaction.index = BigInt.fromI32(1);
 defaultTransaction.from = Address.fromString("123456");
 defaultTransaction.to = Address.fromString("123456");
 defaultTransaction.value = BigInt.fromI32(1);
 defaultTransaction.gasUsed = BigInt.fromI32(1);
 defaultTransaction.gasPrice = BigInt.fromI32(1);
-defaultTransaction.input = typeConversion.stringToH160("1");
+defaultTransaction.input = typeConversion.stringToH160("0x111");
 
-let defaultEventParams: Array<EthereumEventParam>;
+let defaultEventParams: Array<EthereumEventParam> = new Array<EthereumEventParam>();
 
-let eventParam: EthereumEventParam;
+let eventParam: EthereumEventParam = new EthereumEventParam();
 eventParam.name = "name";
 
-let ethereumValue: EthereumValue;
+let ethereumValue: EthereumValue = new EthereumValue();
 ethereumValue.kind = EthereumValueKind.INT;
 ethereumValue.data = 1;
 
 defaultEventParams.push(eventParam);
 
-let defaultEventData: EthereumEvent;
+let defaultEventData: EthereumEvent = new EthereumEvent();
 defaultEventData.address = Address.fromString("123456");
 defaultEventData.logIndex = BigInt.fromI32(1);
 defaultEventData.logType = "default_log_type";
