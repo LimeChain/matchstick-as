1c19cd640fceb37bcf093762fde7f163c667d124
Petko <pavlovskipetko@abv.bg>
2021-07-16 14:18:15 +0300

    Rename

--- assembly/assertFieldEq.ts
+++ assembly/assertFieldEq.ts
@@ -1,5 +1,5 @@
-export declare namespace assert {
+export declare namespace field {
     // Host export for asserting equality on fields in the store
     // TODO: try to make this a bool
-    export function fieldEquals(entityType: string, id: string, fieldName: string, expectedVal: string): i32;
+    export function equals(entityType: string, id: string, fieldName: string, expectedVal: string): i32;
 }
