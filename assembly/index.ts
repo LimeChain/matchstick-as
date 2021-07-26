47315938f04276f606704b0e87e9b3d24d665799
georg-getz <georgi.getz@outlook.com>
2021-07-26 17:37:51 +0300

    changed address type from string to Address and removed reverts bool from mockFunction

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,4 +1,4 @@
-import { ethereum } from "@graphprotocol/graph-ts";
+import { Address, ethereum } from "@graphprotocol/graph-ts";
 import { log } from "./log";
 
 export { clearStore } from "./store";
@@ -8,7 +8,7 @@ export { addMetadata } from "./event";
 const CLASS_IN_FINISHED_STATE_ERROR_MESSAGE = "You can't modify a MockedFunction instance after it has been saved.";
 
 export declare function registerTest(name: string): void;
-export declare function mockFunction(contractAddress: string, fnName: string, fnArgs: ethereum.Value[], returnValue: ethereum.Value, reverts: bool): void;
+export declare function mockFunction(contractAddress: Address, fnName: string, fnArgs: ethereum.Value[], returnValue: ethereum.Value): void;
 
 export function test(name: string, f: () => void): void {
     registerTest(name);
@@ -23,30 +23,15 @@ export class MockedContract {
         contract.address = address;
         return contract;
     }
-
-    // callFunction(fnName: string, fnArgs: string []): string {
-    //     let hash = createHash(this.address, fnName, fnArgs);
-    //     if (hashAndReturnValue.has(hash)) {
-    //         return hashAndReturnValue.get(hash);
-    //     }
-    //     log.error(
-    //         "No function with name '" +
-    //         fnName +
-    //         "', contract address '" +
-    //         this.address +
-    //         "' and given arguments found."
-    //     );
-    //     return "";
-    // }
 }
 
 export class MockedFunction {
     isFinishedState: bool = false;
-    contractAddress: string;
+    contractAddress: Address;
     name: string;
     args: ethereum.Value[];
 
-    constructor(contractAddress: string, fnName: string) {
+    constructor(contractAddress: Address, fnName: string) {
         this.contractAddress = contractAddress;
         this.name = fnName;
     }
@@ -62,7 +47,7 @@ export class MockedFunction {
 
     returns(returnValue: ethereum.Value): void {
         if (!this.isFinishedState) {
-            mockFunction(this.contractAddress, this.name, this.args, returnValue, false);
+            mockFunction(this.contractAddress, this.name, this.args, returnValue);
             this.isFinishedState = true;
         } else {
             log.critical(CLASS_IN_FINISHED_STATE_ERROR_MESSAGE);
@@ -71,7 +56,7 @@ export class MockedFunction {
 
     reverts(): void {
         if (!this.isFinishedState) {
-            mockFunction(this.contractAddress, this.name, this.args, ethereum.Value.fromString(""), true);
+            mockFunction(this.contractAddress, this.name, this.args, ethereum.Value.fromString(""));
             this.isFinishedState = true;
         } else {
             log.critical(CLASS_IN_FINISHED_STATE_ERROR_MESSAGE);
@@ -80,32 +65,8 @@ export class MockedFunction {
 }
 
 export function createMockedFunction(
-    contractAddress: string,
+    contractAddress: Address,
     fnName: string
 ): MockedFunction {
     return new MockedFunction(contractAddress, fnName);
 }
-
-// function createHash(
-//     address: string,
-//     fnName: string,
-//     fnArguments: string[],
-// ): i32 {
-//     let stringToHash = address + fnName;
-//     for (let i = 0; i < fnArguments.length; i++) {
-//         stringToHash.concat(fnArguments[i]);
-//     }
-
-//     let hash = 0;
-//     if (stringToHash.length == 0) {
-//         return hash;
-//     }
-
-//     for (let i = 0; i < stringToHash.length; i++) {
-//         let char = stringToHash.charCodeAt(i);
-//         hash = (hash << 5) - hash + char;
-//         hash = hash & hash;
-//     }
-
-//     return hash;
-// }
