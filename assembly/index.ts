680e52b7e8af3b04a2f78f367b1a9dbc4607308d
Petko <pavlovskipetko@abv.bg>
2021-08-20 13:20:36 +0300

    Clear parameters on event creation.

--- assembly/index.ts
+++ assembly/index.ts
@@ -5,61 +5,88 @@ export { clearStore } from "./store";
 export { assert } from "./assert";
 export { addMetadata } from "./event";
 
-const CLASS_IN_FINISHED_STATE_ERROR_MESSAGE = "You can't modify a MockedFunction instance after it has been saved.";
+const CLASS_IN_FINISHED_STATE_ERROR_MESSAGE =
+  "You can't modify a MockedFunction instance after it has been saved.";
 
 export declare function registerTest(name: string): void;
-export declare function mockFunction(contractAddress: Address, fnName: string, fnSignature: string, fnArgs: ethereum.Value[], returnValue: ethereum.Value[], reverts: u32): void;
+export declare function mockFunction(
+  contractAddress: Address,
+  fnName: string,
+  fnSignature: string,
+  fnArgs: ethereum.Value[],
+  returnValue: ethereum.Value[],
+  reverts: u32
+): void;
 
 export function test(name: string, f: () => void): void {
-    registerTest(name);
-    f();
+  registerTest(name);
+  f();
+}
+
+export function newMockEvent(event: ethereum.Event): ethereum.Event {
+  event.parameters.splice(0, event.parameters.length);
+  return addMetadata(event);
 }
 
 export class MockedFunction {
-    isFinishedState: bool = false;
-    contractAddress: Address;
-    name: string;
-    signature: string;
-    args: ethereum.Value[];
+  isFinishedState: bool = false;
+  contractAddress: Address;
+  name: string;
+  signature: string;
+  args: ethereum.Value[];
 
-    constructor(contractAddress: Address, fnName: string, fnSignature: string) {
-        this.contractAddress = contractAddress;
-        this.name = fnName;
-        this.signature = fnSignature;
-    }
+  constructor(contractAddress: Address, fnName: string, fnSignature: string) {
+    this.contractAddress = contractAddress;
+    this.name = fnName;
+    this.signature = fnSignature;
+  }
 
-    withArgs(args: ethereum.Value[]): MockedFunction {
-        if (!this.isFinishedState) {
-            this.args = args;
-        } else {
-            log.critical(CLASS_IN_FINISHED_STATE_ERROR_MESSAGE);
-        }
-        return this;
+  withArgs(args: ethereum.Value[]): MockedFunction {
+    if (!this.isFinishedState) {
+      this.args = args;
+    } else {
+      log.critical(CLASS_IN_FINISHED_STATE_ERROR_MESSAGE);
     }
+    return this;
+  }
 
-    returns(returnValue: ethereum.Value[]): void {
-        if (!this.isFinishedState) {
-            mockFunction(this.contractAddress, this.name, this.signature, this.args, returnValue, 0);
-            this.isFinishedState = true;
-        } else {
-            log.critical(CLASS_IN_FINISHED_STATE_ERROR_MESSAGE);
-        }
+  returns(returnValue: ethereum.Value[]): void {
+    if (!this.isFinishedState) {
+      mockFunction(
+        this.contractAddress,
+        this.name,
+        this.signature,
+        this.args,
+        returnValue,
+        0
+      );
+      this.isFinishedState = true;
+    } else {
+      log.critical(CLASS_IN_FINISHED_STATE_ERROR_MESSAGE);
     }
+  }
 
-    reverts(): void {
-        if (!this.isFinishedState) {
-            mockFunction(this.contractAddress, this.name, this.signature, this.args, [], 1);
-            this.isFinishedState = true;
-        } else {
-            log.critical(CLASS_IN_FINISHED_STATE_ERROR_MESSAGE);
-        }
+  reverts(): void {
+    if (!this.isFinishedState) {
+      mockFunction(
+        this.contractAddress,
+        this.name,
+        this.signature,
+        this.args,
+        [],
+        1
+      );
+      this.isFinishedState = true;
+    } else {
+      log.critical(CLASS_IN_FINISHED_STATE_ERROR_MESSAGE);
     }
+  }
 }
 
 export function createMockedFunction(
-    contractAddress: Address,
-    fnName: string,
-    fnSignature: string
+  contractAddress: Address,
+  fnName: string,
+  fnSignature: string
 ): MockedFunction {
-    return new MockedFunction(contractAddress, fnName, fnSignature);
+  return new MockedFunction(contractAddress, fnName, fnSignature);
 }
