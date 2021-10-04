6a5c3eceb23f877e0f7a988bfa852bb00a65f555
georg-getz <georgi.getz@outlook.com>
2021-10-04 16:01:28 +0300

    added addressEquals method

--- assembly/assert.ts
+++ assembly/assert.ts
@@ -1,4 +1,4 @@
-import { ethereum } from "@graphprotocol/graph-ts";
+import { Address, BigInt, Bytes, ethereum, store, Value } from "@graphprotocol/graph-ts";
 
 export declare namespace assert {
     // Host export for asserting equality on fields in the store
@@ -7,4 +7,10 @@ export declare namespace assert {
     export function equals(expected: ethereum.Value, actual: ethereum.Value): void;
 
     export function notInStore(entityType: string, id: string): void;
+}
+
+export namespace assert {
+    export function addressEquals(address1: Address, address2: Address): void {
+        assert.equals(ethereum.Value.fromAddress(address1), ethereum.Value.fromAddress(address2));
+    }
 }
