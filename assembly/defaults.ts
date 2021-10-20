2422dd5f6cb045a2cf4ca24664a8bf1ea04a32ad
georg-getz <georgi.getz@outlook.com>
2021-10-20 14:57:50 +0300

    fix type mismatch

--- assembly/defaults.ts
+++ assembly/defaults.ts
@@ -3,7 +3,7 @@ import { Address, BigInt, Bytes, ethereum } from "@graphprotocol/graph-ts";
 let defaultAddress = Address.fromString("0xA16081F360e3847006dB660bae1c6d1b2e17eC2A");
 let defaultAddressBytes = Address.fromString("0xA16081F360e3847006dB660bae1c6d1b2e17eC2A") as Bytes;
 let defaultBigInt = BigInt.fromI32(1);
-let defaultBlock = new ethereum.Block(defaultAddressBytes, defaultAddressBytes, defaultAddressBytes, defaultAddressBytes,
+let defaultBlock = new ethereum.Block(defaultAddressBytes, defaultAddressBytes, defaultAddressBytes, defaultAddress,
     defaultAddressBytes, defaultAddressBytes, defaultAddressBytes, defaultBigInt, defaultBigInt,
     defaultBigInt, defaultBigInt, defaultBigInt, defaultBigInt, defaultBigInt);
 let defaultTransaction = new ethereum.Transaction(defaultAddressBytes, defaultBigInt, defaultAddress,
