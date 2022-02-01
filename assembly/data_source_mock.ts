7b1864ab26a65bca18d633a0da3dc09fd476fe8b
georg-getz <georgi.getz@outlook.com>
2022-02-01 03:07:43 +0200

    added file with datasource return values changing function in it

---
+++ assembly/data_source_mock.ts
@@ -0,0 +1,3 @@
+import { Address, DataSourceContext } from "@graphprotocol/graph-ts";
+
+export declare function setDatasourceReturnValues(address: Address, network: String, context: DataSourceContext): void
