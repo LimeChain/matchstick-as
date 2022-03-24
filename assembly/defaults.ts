90e5b87e2031de081c2f73e990f3d0633fe5523d
georg-getz <georgi.getz@outlook.com>
2022-03-24 17:16:29 +0200

    changed new transaction and block to be separate private functions instead of giving back the same object

--- assembly/defaults.ts
+++ assembly/defaults.ts
@@ -3,18 +3,24 @@ import { Address, BigInt, Bytes, ethereum } from "@graphprotocol/graph-ts";
 let defaultAddress = Address.fromString("0xA16081F360e3847006dB660bae1c6d1b2e17eC2A");
 let defaultAddressBytes = Address.fromString("0xA16081F360e3847006dB660bae1c6d1b2e17eC2A") as Bytes;
 let defaultBigInt = BigInt.fromI32(1);
-let defaultBlock = new ethereum.Block(defaultAddressBytes, defaultAddressBytes, defaultAddressBytes, defaultAddress,
-    defaultAddressBytes, defaultAddressBytes, defaultAddressBytes, defaultBigInt, defaultBigInt,
-    defaultBigInt, defaultBigInt, defaultBigInt, defaultBigInt, defaultBigInt, defaultBigInt);
-let defaultTransaction = new ethereum.Transaction(defaultAddressBytes, defaultBigInt, defaultAddress,
-    defaultAddress, defaultBigInt, defaultBigInt, defaultBigInt, defaultAddressBytes, defaultBigInt);
 let defaultEventDataLogType = "default_log_type";
 
 export function newMockEvent(): ethereum.Event {
     return new ethereum.Event(defaultAddress, defaultBigInt,
-        defaultBigInt, defaultEventDataLogType, defaultBlock, defaultTransaction, []);
+        defaultBigInt, defaultEventDataLogType, newBlock(), newTransaction(), []);
 }
 
 export function newMockCall(): ethereum.Call {
-    return new ethereum.Call(defaultAddress, defaultAddress, defaultBlock, defaultTransaction, [], []);
+    return new ethereum.Call(defaultAddress, defaultAddress, newBlock(), newTransaction(), [], []);
+}
+
+function newBlock(): ethereum.Block {
+    return new ethereum.Block(defaultAddressBytes, defaultAddressBytes, defaultAddressBytes, defaultAddress,
+    defaultAddressBytes, defaultAddressBytes, defaultAddressBytes, defaultBigInt, defaultBigInt,
+    defaultBigInt, defaultBigInt, defaultBigInt, defaultBigInt, defaultBigInt, defaultBigInt);
+}
+
+function newTransaction(): ethereum.Transaction {
+    return new ethereum.Transaction(defaultAddressBytes, defaultBigInt, defaultAddress,
+    defaultAddress, defaultBigInt, defaultBigInt, defaultBigInt, defaultAddressBytes, defaultBigInt);
 }
